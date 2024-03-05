732a6c1aca89c4d99b9d7f1c6cda3320f5785bea
t0muxx <maraval.tom@protonmail.com>
2024-03-05 19:14:27 +0100

    updated process functions

--- process.cpp
+++ process.cpp
@@ -1,17 +1,22 @@
-#include "peb.h"
-#include <cstring>
+#include <cstdint>
 #include <windows.h>
 
+#include "peb.h"
+#include <cstring>
+
+#include <Psapi.h>
 #include <TlHelp32.h>
 #include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
 
+#include "errors.h"
+
 namespace process {
 
 PPEB getPEB() { return (PPEB)__readgsqword(0x60); }
 
-uintptr_t get_module_ba(DWORD pid, const char *modName) {
+uintptr_t get_module_ba_from_pid(DWORD pid, const char *modName) {
   uintptr_t modBaseAddr = 0;
   HANDLE hSnap =
       CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
@@ -29,6 +34,27 @@ uintptr_t get_module_ba(DWORD pid, const char *modName) {
   }
   CloseHandle(hSnap);
   return modBaseAddr;
+}
+
+uintptr_t get_exe_ba(HANDLE proc) {
+  if (proc == (HANDLE)NULL) {
+    return (NULL);
+  }
+
+  HMODULE modules[1024] = {0};
+  DWORD bytes_needed = 0;
+
+  if (!EnumProcessModules(proc, modules, sizeof(modules), &bytes_needed)) {
+    errors::error_print("EnumProcessModules");
+    return (NULL);
+  }
+
+  char mod_name[MAX_PATH];
+  if (!GetModuleFileNameExA(proc, modules[0], mod_name, sizeof(mod_name))) {
+    errors::error_print("EnumProcessModules");
+    return (NULL);
+  }
+  return (uintptr_t)modules[0];
 }
 
 DWORD pid_from_windows_title(LPCSTR windowTitle) {
