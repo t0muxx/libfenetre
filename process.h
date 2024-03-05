3455f015d8bbf59fb75f47d563b44db67832c86a
t0muxx <maraval.tom@protonmail.com>
2024-03-05 19:14:31 +0100

    updated process functions

--- process.h
+++ process.h
@@ -7,6 +7,7 @@ namespace process {
 PPEB getPEB();
 
 uintptr_t get_module_ba(DWORD pid, const char *modName);
+uintptr_t get_exe_ba(HANDLE proc);
 DWORD pid_from_windows_title(LPCSTR windowTitle);
 DWORD pid_from_processname(LPCSTR execName);
 
